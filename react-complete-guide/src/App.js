f9a061da6408124c2ae15c460efc99fc5ad24f53
TanuAgrawal123 <tanuagrawal1389@gmail.com>
2019-07-20 08:13:21 +0530

    passing methods reference between component and adding two way binding

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -11,26 +11,34 @@ class App extends Component{
     otherstate:'some other value'
     
     }
-    switchNameHandler=()=>{
+    switchNameHandler=(newname)=>{
      // console.log("It works");
      // Dont't Do This ... this.state.person[0].name="tanu agrawal"
      this.setState({
       person:[
-        {name:"tanu agrawal",age:21},
+        {name:newname,age:21},
         {name:"anubha", age:23},
         {name:"harsh", age:18}
       ]
      })
 
   }
-
+   nameChangeHandler=(event)=>{
+    this.setState({
+      person:[
+        {name:"tanu",age:21},
+        {name:event.target.value, age:23},
+        {name:"harsh", age:18}
+      ]
+     })
+   }
  render(){
   return(
       <div className="App">
       <h1>Hi I m react app</h1>
       <h1 onCopy={this.switchNameHandler}>Switch here!</h1>
       <Person name={this.state.person[0].name} age={this.state.person[0].age}/>
-      <Person name={this.state.person[1].name} age={this.state.person[1].age}> My hobby is cooking!</Person>
+      <Person name={this.state.person[1].name} age={this.state.person[1].age} click={this.switchNameHandler.bind(this,"Tanu Agrawal")} changed={this.nameChangeHandler}> My hobby is not  cooking!  </Person>
       <Person name={this.state.person[2].name} age={this.state.person[2].age}/>
 
       </div>
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import './Person.css';
 const person=(props)=>{
-    return <div><p>I am {props.name} and  I am {props.age} years old!!!</p>
-    <p>{props.children}</p></div>
+    return <div className="Person"><p>I am {props.name} and  I am {props.age} years old!!!</p>
+    <input type="text" onChange={props.changed} value={props.name}/>
+    <p onClick={props.click}></p>
+    
+    </div>
 }
 
  export default person;
